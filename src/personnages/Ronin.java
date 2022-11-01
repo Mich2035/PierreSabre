7c47672ab64a8c1fefc41f889008e2bf8666b602
unknown <mbeutoum_michele@yahoo.com>
2022-11-02 00:44:56 +0100

    Duel entre Ronin et Yakuza

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -15,9 +15,31 @@ public class Ronin extends Humain {
 
 
 	public void donnner(Commercant beneficiaire) {
+		int econ=getArgent();
 		int argentdonné=(getArgent()*10)/100;
 		super.parler(beneficiaire.getNom()+" prend ces "+argentdonné+" sous");
 		beneficiaire.recevoir(argentdonné);
+		econ-=argentdonné;
 	}
+	public void provoquer(Yakuza adversaire) {
+		int force=2*honneur;
+		if (force>=adversaire.getReputation()) {
+			parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fais à ce pauvre marchand!");
+			parler("Je t'ai eu petit yakusa");
+			super.gagnerArgent(adversaire.getArgent());
+			adversaire.perdre();
+			honneur+=1;
+		
+			
+		} else {
+			honneur-=1;
+			parler("J'ai perdu contre ce yakuza,mon honneur et ma bourse ont en pris un coup");
+			super.perteArgent(getArgent());
+			adversaire.gagner(getArgent());
+				
+			}
+			
+		}
+	}
+	
 
-}
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -2,11 +2,12 @@ package personnages;
 
 public class Yakuza extends Humain {
 	private String clan;
-	private int reputation=0;
+	private int reputation=4;
 
 	public Yakuza(String nom,String boisson,int argent,String clan) {
 		
 		super(nom,boisson,argent);
+		this.clan=clan;
 	}
 	@Override
 	public int getArgent() {
@@ -27,6 +28,12 @@ public class Yakuza extends Humain {
 		super.parler("Marco,si tu tiens à la vie donne moi ta bourse");
 		
 	}
+	public String getClan() {
+		return clan;
+	}
+	public int getReputation() {
+		return reputation;
+	}
 
 	@Override
 	public void direBonjour() {
@@ -34,12 +41,28 @@ public class Yakuza extends Humain {
 		super.direBonjour();
 	}
 	public void extorquer(Commercant victime) {
+	
 		int argentVolé=victime.seFaireExtorquer();
 		int argent=super.gagnerArgent(argentVolé);
 		parler("J'ai piqué les "+argentVolé+" sous de "+victime.getNom()+" ce qui me fait "+argent+" sous dans ma poche.Hi!Hi!");
 		reputation+=1;
+		
 	}
 
+	public int perdre() {
+		int nouv=getArgent();
+		super.perteArgent(getArgent());
+		parler("J'ai perdu mon duel et mes "+nouv+" sous,snif...J'ai déshonoré le clan de "+getClan());
+		return reputation-=1;
+	}
 
+	public int gagner(int gain) {
+		int econ=getArgent();
+		//int nouv=super.gagnerArgent();
+		int nouv=gagnerArgent(9);
+		parler("ce ronin pensait vraiment battre "+getNom()+"du clan "+getClan()+"? je l'ai depouillé de ses "+nouv+" sous");
+		
+		return reputation+=1;
+	}
 
 }
